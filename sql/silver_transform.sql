83674bfae0118e0d79c4f3b74f259780dfa3680a
mbarbacardozo <mbarbacardozo@gmail.com>
2026-06-06 00:04:00 -0400

    fix(silver): alinear silver_transform.sql con contrato extract.py

--- sql/silver_transform.sql
+++ sql/silver_transform.sql
@@ -3,7 +3,7 @@
 -- PURPOSE: Limpia y estructura datos crudos de Open-Meteo
 --          Bronze (GCS) -> Silver (BigQuery)
 -- AUTHOR:  Montserrat Barba
--- VERSION: 1.0
+-- VERSION: 1.1
 -- ============================================================
 
 -- ---------------------------------------------------------
@@ -11,27 +11,28 @@
 -- ---------------------------------------------------------
 CREATE OR REPLACE TABLE `silver.daily_weather_clean` AS
 
-WITH raw_data AS (
+WITH exploded AS (
   SELECT
-    JSON_VALUE(data, '$.meta.city_id')                AS city_id,
-    CAST(daily.date AS DATE)                          AS date,
-    CAST(daily.temperature_2m_max AS FLOAT64)         AS temperature_2m_max,
-    CAST(daily.temperature_2m_min AS FLOAT64)         AS temperature_2m_min,
-    CAST(daily.temperature_2m_mean AS FLOAT64)        AS temperature_2m_mean,
-    CAST(daily.precipitation_sum AS FLOAT64)          AS precipitation_sum,
-    CAST(daily.windspeed_10m_max AS FLOAT64)          AS windspeed_10m_max,
-    CAST(daily.weathercode AS INT64)                  AS weathercode,
-    CAST(daily.et0_fao_evapotranspiration AS FLOAT64) AS et0_fao_evapotranspiration,
+    JSON_VALUE(data, '$.meta.city_id')                    AS city_id,
+    DATE(date_str)                                        AS date,
+    CAST(JSON_VALUE(data, CONCAT('$.data.daily.temperature_2m_max[', idx, ']')) AS FLOAT64) AS temperature_2m_max,
+    CAST(JSON_VALUE(data, CONCAT('$.data.daily.temperature_2m_min[', idx, ']')) AS FLOAT64) AS temperature_2m_min,
+    CAST(JSON_VALUE(data, CONCAT('$.data.daily.temperature_2m_mean[', idx, ']')) AS FLOAT64) AS temperature_2m_mean,
+    CAST(JSON_VALUE(data, CONCAT('$.data.daily.precipitation_sum[', idx, ']')) AS FLOAT64) AS precipitation_sum,
+    CAST(JSON_VALUE(data, CONCAT('$.data.daily.windspeed_10m_max[', idx, ']')) AS FLOAT64) AS windspeed_10m_max,
+    CAST(JSON_VALUE(data, CONCAT('$.data.daily.weathercode[', idx, ']')) AS INT64) AS weathercode,
+    CAST(JSON_VALUE(data, CONCAT('$.data.daily.et0_fao_evapotranspiration[', idx, ']')) AS FLOAT64) AS et0_fao_evapotranspiration,
+    JSON_VALUE(data, '$.meta.extraction_timestamp_utc')   AS extraction_timestamp_utc,
     ROW_NUMBER() OVER (
-      PARTITION BY JSON_VALUE(data, '$.meta.city_id'), daily.date
-      ORDER BY JSON_VALUE(data, '$.meta.extracted_at') DESC
+      PARTITION BY JSON_VALUE(data, '$.meta.city_id'), DATE(date_str)
+      ORDER BY JSON_VALUE(data, '$.meta.extraction_timestamp_utc') DESC
     ) AS row_num
   FROM `bronze.raw_weather`,
-    UNNEST(JSON_QUERY_ARRAY(data, '$.data.daily')) AS daily
+    UNNEST(JSON_VALUE_ARRAY(data, '$.data.daily.time')) AS date_str WITH OFFSET AS idx
 ),
 
 validated AS (
-  SELECT * FROM raw_data
+  SELECT * FROM exploded
   WHERE
     row_num = 1
     AND city_id IS NOT NULL
@@ -46,47 +47,47 @@ SELECT
   city_id, date,
   temperature_2m_max, temperature_2m_min, temperature_2m_mean,
   precipitation_sum, windspeed_10m_max, weathercode,
-  et0_fao_evapotranspiration
+  et0_fao_evapotranspiration,
+  extraction_timestamp_utc
 FROM validated;
 
 
 -- ---------------------------------------------------------
 -- TABLA 2: silver.quarantine_log
--- Registros rechazados con motivo documentado
 -- ---------------------------------------------------------
 CREATE OR REPLACE TABLE `silver.quarantine_log` AS
 
 SELECT
-  JSON_VALUE(data, '$.meta.city_id')           AS city_id,
-  CAST(daily.date AS DATE)                      AS date,
-  CAST(daily.temperature_2m_max AS FLOAT64)     AS temperature_2m_max,
-  CAST(daily.temperature_2m_min AS FLOAT64)     AS temperature_2m_min,
-  CAST(daily.precipitation_sum AS FLOAT64)      AS precipitation_sum,
+  JSON_VALUE(data, '$.meta.city_id')       AS city_id,
+  DATE(date_str)                            AS date,
+  CAST(JSON_VALUE(data, CONCAT('$.data.daily.temperature_2m_max[', idx, ']')) AS FLOAT64) AS temperature_2m_max,
+  CAST(JSON_VALUE(data, CONCAT('$.data.daily.temperature_2m_min[', idx, ']')) AS FLOAT64) AS temperature_2m_min,
+  CAST(JSON_VALUE(data, CONCAT('$.data.daily.precipitation_sum[', idx, ']')) AS FLOAT64)  AS precipitation_sum,
   CASE
-    WHEN JSON_VALUE(data, '$.meta.city_id') IS NULL            THEN 'city_id nulo'
-    WHEN daily.date IS NULL                                    THEN 'date nulo'
-    WHEN CAST(daily.temperature_2m_max AS FLOAT64) IS NULL     THEN 'temperature_max nulo'
-    WHEN CAST(daily.precipitation_sum AS FLOAT64) IS NULL      THEN 'precipitation nulo'
-    WHEN CAST(daily.temperature_2m_max AS FLOAT64)
-       < CAST(daily.temperature_2m_min AS FLOAT64)             THEN 'temp_max < temp_min'
-    WHEN CAST(daily.precipitation_sum AS FLOAT64) < 0          THEN 'precipitacion negativa'
+    WHEN JSON_VALUE(data, '$.meta.city_id') IS NULL THEN 'city_id nulo'
+    WHEN DATE(date_str) IS NULL                     THEN 'date nulo'
+    WHEN CAST(JSON_VALUE(data, CONCAT('$.data.daily.temperature_2m_max[', idx, ']')) AS FLOAT64) IS NULL THEN 'temperature_max nulo'
+    WHEN CAST(JSON_VALUE(data, CONCAT('$.data.daily.precipitation_sum[', idx, ']')) AS FLOAT64) IS NULL  THEN 'precipitation nulo'
+    WHEN CAST(JSON_VALUE(data, CONCAT('$.data.daily.temperature_2m_max[', idx, ']')) AS FLOAT64)
+       < CAST(JSON_VALUE(data, CONCAT('$.data.daily.temperature_2m_min[', idx, ']')) AS FLOAT64) THEN 'temp_max < temp_min'
+    WHEN CAST(JSON_VALUE(data, CONCAT('$.data.daily.precipitation_sum[', idx, ']')) AS FLOAT64) < 0 THEN 'precipitacion negativa'
     ELSE 'otro'
   END AS rejection_reason,
   CURRENT_TIMESTAMP() AS quarantined_at
 FROM `bronze.raw_weather`,
-  UNNEST(JSON_QUERY_ARRAY(data, '$.data.daily')) AS daily
+  UNNEST(JSON_VALUE_ARRAY(data, '$.data.daily.time')) AS date_str WITH OFFSET AS idx
 WHERE
   JSON_VALUE(data, '$.meta.city_id') IS NULL
-  OR daily.date IS NULL
-  OR CAST(daily.temperature_2m_max AS FLOAT64) IS NULL
-  OR CAST(daily.precipitation_sum AS FLOAT64) IS NULL
-  OR CAST(daily.temperature_2m_max AS FLOAT64) < CAST(daily.temperature_2m_min AS FLOAT64)
-  OR CAST(daily.precipitation_sum AS FLOAT64) < 0;
+  OR DATE(date_str) IS NULL
+  OR CAST(JSON_VALUE(data, CONCAT('$.data.daily.temperature_2m_max[', idx, ']')) AS FLOAT64) IS NULL
+  OR CAST(JSON_VALUE(data, CONCAT('$.data.daily.precipitation_sum[', idx, ']')) AS FLOAT64) IS NULL
+  OR CAST(JSON_VALUE(data, CONCAT('$.data.daily.temperature_2m_max[', idx, ']')) AS FLOAT64)
+   < CAST(JSON_VALUE(data, CONCAT('$.data.daily.temperature_2m_min[', idx, ']')) AS FLOAT64)
+  OR CAST(JSON_VALUE(data, CONCAT('$.data.daily.precipitation_sum[', idx, ']')) AS FLOAT64) < 0;
 
 
 -- ---------------------------------------------------------
 -- TABLA 3: silver.cities_dim
--- Dimension estatica de ciudades bolivianas
 -- ---------------------------------------------------------
 CREATE OR REPLACE TABLE `silver.cities_dim` AS
 
